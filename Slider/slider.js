db274d8a912180cf24e5747d4eb01f3934f18f7a
Reson-a <80547665@qq.com>
2016-09-08 21:55:12 +0800

    slider v1

---
+++ Slider/slider.js
@@ -0,0 +1,205 @@
+/**
+ * Created by Reson-a on 2016/9/6.
+ * slider组件脚本
+ */
+(function(_) {
+    var sliderTemplate = '<div class="m-slider">\
+        <div class="slide"></div>\
+        <div class="slide"></div>\
+        <div class="slide"></div>\
+        </div>';
+
+    var cursorTemplate = '<ul class="m-cursor"><li class="prev">&lt;</li><li class="next">&gt;</li></ul>';
+
+    function Slider(options) {
+        if (options) _.extend(this, options);
+        this.container = this.container || document.body;
+        this.container.style.overflow = 'hidden';
+
+        this.slider = this._layout.cloneNode(true); //
+        this.slides = [].slice.call(this.slider.querySelectorAll('.slide'), 0);
+        this.slidesNum = this.slides.length;
+
+        this.container.appendChild(this.slider);
+
+        this.pageNum = this.images.length; //图片总数
+        this.pageIndex = this.pageIndex || 0; //当前页码
+
+        if (this.isCursor) this._initCursor();
+        if (this.isDrag) this._initDrag();
+        if (this.isAutoPlay) this._initAutoPlay();
+
+        // 拖拽相关
+        this.offsetWidth = this.container.offsetWidth;
+        this.breakPoint = this.offsetWidth / 4;
+
+        this._step(0);
+
+    };
+
+    _.extend(Slider.prototype, _.emitter);
+
+    _.extend(Slider.prototype, {
+        _layout: _.html2node(sliderTemplate),
+        //导航向指定页
+        nav: function(pageIndex) {
+            this.pageIndex = pageIndex;
+            for (var i = 0, l = this.slidesNum; i < l; i++) {
+                this.slides[i].style.transitionDuration = '0s'; //直接导航时 不播放动画
+            }
+            this._onNav();
+        },
+        prev: function() {
+            this._step(-1);
+        },
+        next: function() {
+            this._step(1);
+        },
+        _step: function(num) {
+            this.pageIndex = this._normal(this.pageIndex + num, this.pageNum);
+
+            for (var i = 0; i < this.slidesNum; i++) {
+                if (this.slides[i].posIndex === undefined) { //动画初始化操作
+                    this.slides[i].posIndex = i - 1;
+                    this.slides[i].style.transitionDuration = '0s';
+                } else if (!num) this.slides[i].style.transitionDuration = '.5s'; //用于拖拽回弹
+                else if (this.slides[i].posIndex + num) this.slides[i].style.transitionDuration = '.5s'; //翻页时播放动画
+                else this.slides[i].style.transitionDuration = '0s';
+
+                var index = this.slides[i].posIndex - num;
+                if (index < -1) index = 1;
+                else if (index > 1) index = -1;
+                this.slides[i].style.left = index * 100 + '%';
+                this.slides[i].posIndex = index;
+            }
+            this._onNav();
+        },
+
+
+        _onNav: function() { //图片状态的更新 slider=1 对应当前图片,在nav和_step后调用
+            for (var i = 0; i < this.slidesNum; i++) {
+                var img = this.slides[i].querySelector('img');
+                if (!img) {
+                    img = document.createElement('img');
+                    this.slides[i].appendChild(img);
+                }
+                img.src = this.images[this._normal(this.pageIndex + this.slides[i].posIndex, this.pageNum)];
+            }
+            if (this.isCursor) this._upDateCursor();
+        },
+
+        //处理下标的方法
+        _normal: function(index, length) {
+            return (index + length) % length;
+        }
+    });
+
+
+    _.extend(Slider.prototype, {
+        //初始化cursor
+        _initCursor: function() {
+            this.m_cursor = _.html2node(cursorTemplate);
+            this.cursors = [];
+            //添加前后翻页
+            var prev = this.m_cursor.firstChild,
+                next = this.m_cursor.lastChild;
+            addEvent(prev, 'click', this.prev.bind(this));
+            addEvent(next, 'click', this.next.bind(this));
+
+            for (var i = 0; i < this.pageNum; i++) {
+                var cursor = document.createElement('li');
+                cursor.className = 'cursor';
+                cursor.textContent = i + 1;
+                this.cursors.push(cursor)
+                addEvent(cursor, 'click', this.nav.bind(this, i));
+                this.m_cursor.insertBefore(cursor, this.m_cursor.lastChild);
+            }
+
+            //清除选区
+            addEvent(this.m_cursor, 'mousemove', this._clearSelection.bind(this));
+            addEvent(this.m_cursor, 'click', this._clearSelection.bind(this));
+
+            this.container.appendChild(this.m_cursor);
+        },
+
+        //更新cursor状态,在onNav中调用
+        _upDateCursor: function() {
+            for (var i = 0; i < this.pageNum; i++) {
+                if (this.pageIndex === i) _.addClass(this.cursors[i], 'active');
+                else _.delClass(this.cursors[i], 'active');
+            }
+        },
+        //清除选区
+        _clearSelection() {
+            if (window.getSelection) window.getSelection().removeAllRanges();
+            else if (window.document.selection) window.document.selection.empty();
+        }
+    });
+
+    _.extend(Slider.prototype, {
+        //初始化autoPlay
+        _initAutoPlay: function() {
+            addEvent(this.container, 'mouseout', this.setAutoPlay.bind(this));
+            addEvent(this.container, 'mouseover', this.clearAutoPlay.bind(this));
+        },
+
+        setAutoPlay: function() {
+            if (!this.intervalID) this.intervalID = setInterval(this.next.bind(this), 3000);
+        },
+
+        clearAutoPlay: function() {
+            if (this.intervalID) clearInterval(this.intervalID);
+            this.intervalID = undefined;
+        },
+    });
+
+
+    _.extend(Slider.prototype, {
+        //初始化拖拽
+        _initDrag: function() {
+            this.dragInfo = {};
+            for (var i = 0; i < this.slidesNum; i++) {
+                this.slides[i].style.transitionDuration = '0s';
+            }
+            addEvent(this.slider, 'mousedown', this._dragStart.bind(this));
+            addEvent(this.slider, 'mousemove', this._dragMove.bind(this));
+            addEvent(this.slider, 'mouseup', this._dragEnd.bind(this));
+            addEvent(this.slider, 'mouseleave', this._dragEnd.bind(this));
+        },
+
+        _dragStart: function(event) {
+            this.dragInfo.start = { x: event.pageX };
+            event.preventDefault();
+            return false;
+        },
+
+        _dragMove: function(event) {
+            if (!this.dragInfo.start) return;
+            event.preventDefault();
+            this._clearSelection();
+
+            this.offsetX = event.pageX - this.dragInfo.start.x;
+
+            for (var i = 0; i < this.slidesNum; i++) {
+                this.slides[i].style.transitionDuration = '0s';
+                this.slides[i].style.left = (this.slides[i].posIndex + this.offsetX / this.slides[i].firstChild.width) * 100 + '%';
+            }
+            return false;
+        },
+
+        _dragEnd: function(event) {
+            if (!this.dragInfo.start) return;
+            this.dragInfo = {};
+            event.preventDefault();
+
+            if (this.offsetX > this.breakPoint) this._step(-1);
+            else if (this.offsetX < -this.breakPoint) this._step(1);
+            else this._step(0);
+            return false;
+        }
+
+    });
+
+    //暴露到全局
+    window.Slider = Slider;
+}(utils))
---
+++ Slider/utils.js
@@ -0,0 +1,97 @@
+/**
+ * Created by Reson-a on 2016/9/5.
+ * 自行封装的常用工具，主要学习和借鉴自leeluoele大神 https://github.com/leeluolee
+ */
+//以下代码使用频率较高，直接暴露到全局，请注意变量名污染
+function addEvent(node, event, handler) {
+    if (node.addEventListener) node.addEventListener(event, handler, false);
+    else node.attachEvent('on' + event, handler);
+}
+
+//全局下的类选择器，如果只有一个元素则返回该元素，
+function $(selector) {
+    var elements = [].slice.call(document.querySelectorAll(selector), 0);
+    if (elements.length === 1) return elements[0];
+    else return elements;
+}
+
+//封装的工具，避免全局变量污染
+var utils = function () {
+    return {
+        //添加对应类名，低版本classList.add()方法可能不兼容
+        addClass: function (node, className) {
+            var cur = node.className || "";
+            if ((" " + cur + " ").indexOf(" " + className + " ") === -1)
+                node.className = cur ? cur + " " + className : className;
+        }
+        ,
+
+        //删除对应类名
+        delClass: function (node, className) {
+            var cur = node.className || "";
+            node.className = (" " + cur + " ").replace(" " + className + " ", " ").trim();
+        }
+        ,
+
+        //扩展原型的方法 mixin模式
+        extend: function (o1, o2) {
+            for (var i in o2) {
+                if (o1[i] === undefined) o1[i] = o2[i];
+            }
+        }
+        ,
+
+        //添加自定义监听事件
+        emitter: {
+            //注册事件
+            on: function (event, fn) {
+                var handles = this._handles || (this.handles = {}),
+                    calls = handles[event] || (handles[event] = []);
+                calls.push(fn);
+                return this;
+            }
+            ,
+            //解绑事件
+            off: function (event, fn) {
+                if (!event || !this._handles) this._handles = {};
+                var handles = this._handles, calls;
+                if (calls = handle[event]) {//handle[event]存在，为calls赋值并进入后续判断
+                    if (!fn) {
+                        handles[event] = [];
+                        return this;
+                    }
+                    for (var i = 0, len = calls.length; i < len; i++) {
+                        if (fn === calls[i]) {
+                            calls.splice(i, 1);
+                            return this;
+                        }
+                    }
+                }
+                return this;
+            }
+            ,
+            //触发事件
+            emit: function (event) {
+                var args = [].slice.call(arguments, 1),
+                    handles = this._handles, call;
+                if (!handles || !(calls = handles[event])) return this;
+                for (var i = 0, len = calls.length; i < len; i++) {
+                    calls[i].apply(this, args)
+                }
+                return this;
+            }
+        }
+        ,
+
+        //将html字符串转换成节点
+        html2node: function (str) {
+            var container = document.createElement('div');
+            container.innerHTML = str;
+            return container.children[0];
+        }
+    }
+} ();
+
+
+
+
