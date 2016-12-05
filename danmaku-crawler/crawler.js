7ee202171b4955cdb354bbe4cd05d856f5ec1157
Reson-a <80547665@qq.com>
2016-12-05 23:53:31 +0800

    抓取弹幕

--- danmaku-crawler/crawler.js
+++ danmaku-crawler/crawler.js
@@ -1,50 +1,53 @@
 'use strict'
 
 //修改av为对应视频编号即可
-const av = 'av7260153'
+const avID = 'av7260153'
 
-const http = require('http');
+const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const util = require('util');
 const path = require('path');
-const url = 'http://www.bilibili.com/video/' + av + '/';
+const url = 'http://www.bilibili.com/video/' + avID + '/';
 
-http.get(url, function(res) {
-    let html = '';
-    res.on('data', (data) => { html += data; });
-
-    res.on('end', () => {
-        let data = danmakuFilter(html);
-        let dir = path.resolve(_dirName)
-        writeData(dir, data.data);
-    })
-}).on('error', (err) => { console.log(err); });
-
-function danmakuFilter(html) {
-    let $ = cheerio.load(html);
-    let danmakus = $('.danmaku-info-row');
-    let titleName = $('.v-title');
-    let danmakuData = [];
-
-    danmakus.each(function(item) {
-        let danmaku = $(this);
-        let time = danmaku.find('.danmaku-info-time').text();
-        let date = danmaku.find('.danmaku-info-date').text();
-        let content = danmaku.find('.danmaku-info-danmaku').text();
-        danmakuData.push({
-            time,
-            date,
-            content
-        });
-    });
-    return {
-        titleName,
-        danmakuData
-    };
+const options = {
+    url,
+    gzip: true,
+    method: 'GET',
+    encoding: 'utf-8'
 }
 
-function writeData(path, data) {
+
+let videoMsg = {
+    title: '',
+    author: '',
+    category: '',
+    time: '',
+    avID,
+    chatID: '',
+    danmaku: []
+};
+
+
+//获取视频名称及chaiID
+request(options, function(err, res, body) {
+    getVideoMsg(body);
+    //let dir = path.resolve(__dirname, danmaku.name + '.txt');
+    //writeData(dir, danmaku.data);
+
+}).on('error', (err) => { console.log(err); });
+
+function getVideoMsg(html) {
+    let $ = cheerio.load(html);
+    videoMsg.title = $('.v-title').text();
+    videoMsg.author = $('.usname .name').text();
+    videoMsg.time = $('.tminfo time i').text();
+    videoMsg.category = $('.tminfo span a').text();
+    videoMsg.chatID = $('script').text().constructor.toString();
+    console.log(videoMsg);
+}
+
+function writeData(path, data) { //将数据作为字符串写入文件
     fs.writeFile(path, util.inspect(data), (err) => {
         if (err) return console.log(err);
     });
